780d69fbc16726319143d55532a22be771deb7fa
Marius Paavel <marius.paavel@gmail.com>
2021-03-26 10:24:18 +0000

    commit

--- src/main/java/com/mariuspaavel/javautilities/NumberSerializer.java
+++ src/main/java/com/mariuspaavel/javautilities/NumberSerializer.java
@@ -1,65 +1,72 @@
 package com.mariuspaavel.javautilities;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 
 public class NumberSerializer {
 
-	public static boolean fromByteArray(byte[] bytes) {
+	public static boolean byteArrayToBoolean(byte[] bytes) {
 		return bytes[0] != 0;
 	}
-	public static boolean readBoolean(ByteArrayInputStream stream) {
+	public static boolean readBoolean(InputStream stream) throws IOException{
 		return stream.read() != 0;
 	}
 	public static byte[] booleanToByteArray(boolean value) {
 		return new byte[] {value ? (byte)1 : (byte)0};
 	}
-	public static void writeBoolean(boolean value, ByteArrayOutputStream stream) {
+	public static void writeBoolean(boolean value, OutputStream stream) throws IOException {
 		stream.write(value ? 1 : 0);
 	}
 	
+
 	
 	public static byte byteArrayToByte(byte[] bytes) {
 		return bytes[0];
 	}
-	public static byte readByte(ByteArrayInputStream stream) {
+	public static byte readByte(InputStream stream) throws IOException{
 		return (byte)stream.read();
 	}
 	public static byte[] byteToByteArray(byte value) {
 		return new byte[] {value};
 	}
-	public static void writeByte(byte value, ByteArrayOutputStream stream) {
+	public static void writeByte(byte value, OutputStream stream) throws IOException{
 		stream.write(value & 0xFF);
 	}
 	
+
 	
 	public static int byteArrayToShort(byte[] bytes) {
 		return ((bytes[0] & 0xFF) << 8 ) |
 				(bytes[1] & 0xFF);
 	}
-	public static short readShort(ByteArrayInputStream stream) {
+	public static short readShort(InputStream stream) throws IOException {
 		short output = 0;
 		for(int byteI = 1; byteI >= 0; byteI--) {
 			output |= stream.read() << byteI*8;
 		}
 		return output;
 	}
-	
 	public static byte[] shortToByteArray(short value) {
 	    return new byte[] {
 	            (byte)(value >>> 8),
 	            (byte)value};
 	}
-	
+	public static void writeShort(short value, OutputStream stream) throws IOException{
+		for(int byteI = 0; byteI < 2; byteI++) {
+			stream.write((byte)(value >>> byteI*8));
+		}
+	}
+
+
+
 	public static int byteArrayToInt(byte[] bytes) {
 		return ((bytes[0] & 0xFF) << 24) |
 				((bytes[1] & 0xFF) << 16) |
 				((bytes[2] & 0xFF) << 8 ) |
 				((bytes[3] & 0xFF) << 0 );
 	}
-
-	public static int readInt(ByteArrayInputStream stream) {
+	public static int readInt(InputStream stream) throws IOException{
 		int output = 0;
 		for(int byteI = 3; byteI >= 0; byteI--) {
 			output |= stream.read() << byteI*8;
@@ -75,12 +82,13 @@ public class NumberSerializer {
 	            (byte)value};
 	}
 
-	public static void writeInt(int value, ByteArrayOutputStream stream) {
+	public static void writeInt(int value, OutputStream stream) throws IOException{
 		for(int byteI = 0; byteI < 4; byteI++) {
 			stream.write((byte)(value >>> byteI*8));
 		}
 	}
 	
+
 
 	public static long byteArrayToLong(byte[] bytes) {
 		return ((bytes[0] & 0xFFl) << 56) |
@@ -92,7 +100,7 @@ public class NumberSerializer {
 				((bytes[6] & 0xFFl) << 8 ) |
 				((bytes[7] & 0xFFl) << 0 );
 	}
-	public static int readLong(ByteArrayInputStream stream) {
+	public static int readLong(InputStream stream) throws IOException{
 		int output = 0;
 		for(int byteI = 7; byteI >= 0; byteI--) {
 			output |= stream.read() << byteI*8;
@@ -112,7 +120,7 @@ public class NumberSerializer {
 	            (byte)value};
 	}
 
-	public static void writeLong(long value, ByteArrayOutputStream stream) {
+	public static void writeLong(long value, OutputStream stream) throws IOException{
 		for(int byteI = 0; byteI < 8; byteI++) {
 			stream.write((byte)(value >>> byteI*8));
 		}
@@ -122,13 +130,13 @@ public class NumberSerializer {
 	public static float byteArrayToFloat(byte[] bytes) {
 		return Float.intBitsToFloat(byteArrayToInt(bytes));
 	}
-	public static float readFloat(ByteArrayInputStream stream) {
+	public static float readFloat(InputStream stream) throws IOException {
 		return Float.intBitsToFloat(readInt(stream));
 	}
 	public static byte[] floatToByteArray(float value) {
 		return intToByteArray(Float.floatToIntBits(value));
 	}
-	public static void writeFloat(float value, ByteArrayOutputStream stream) throws IOException {
+	public static void writeFloat(float value, OutputStream stream) throws IOException {
 		writeInt(Float.floatToIntBits(value), stream);
 	}
 	
@@ -136,13 +144,13 @@ public class NumberSerializer {
 	public static float byteArrayToDouble(byte[] bytes) {
 		return Double.doubleToLongBits(byteArrayToLong(bytes));
 	}
-	public static double readDouble(ByteArrayInputStream stream) {
+	public static double readDouble(InputStream stream) throws IOException{
 		return Double.longBitsToDouble(readLong(stream));
 	}
 	public static byte[] doubleToByteArray(double value) {
 		return longToByteArray(Double.doubleToLongBits(value));
 	}
-	public static void writeDouble(double value, ByteArrayOutputStream stream) throws IOException {
+	public static void writeDouble(double value, OutputStream stream) throws IOException {
 		writeLong(Double.doubleToLongBits(value), stream);
 	}
 	
